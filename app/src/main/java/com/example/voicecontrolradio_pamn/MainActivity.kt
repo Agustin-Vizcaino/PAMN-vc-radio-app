16dedcab5a5fc1f3de5a77cea7f2bc32fde94ada
Agustin-Vizcaino <agustin.vizcaino101@alu.ulpgc.es>
2025-01-13 17:26:03 +0000

    Applied custom theme, added test to make sure it works

--- app/src/main/java/com/example/voicecontrolradio_pamn/MainActivity.kt
+++ app/src/main/java/com/example/voicecontrolradio_pamn/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.voicecontrolradio_pamn.ui.theme.VoiceControlRadioPAMNTheme
+import com.example.voicecontrolradio_pamn.ui.theme.app.GlobalColorsPalette
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,10 +34,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Row(modifier = modifier) {
+        Text(
+            text = "Hello ",
+            modifier = modifier
+        )
+        Text(
+            text = name,
+            modifier = modifier,
+            color = GlobalColorsPalette.current.activeText)
+        Text(
+            text = "!",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/voicecontrolradio_pamn/ui/theme/Theme.kt
+++ app/src/main/java/com/example/voicecontrolradio_pamn/ui/theme/Theme.kt
@@ -9,7 +9,10 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.platform.LocalContext
+import com.example.voicecontrolradio_pamn.ui.theme.app.DefaultGlobalPalette
+import com.example.voicecontrolradio_pamn.ui.theme.app.GlobalColorsPalette
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -21,23 +24,13 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
 fun VoiceControlRadioPAMNTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    // Para asegurarnos de usar nuestros colores
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -50,9 +43,21 @@ fun VoiceControlRadioPAMNTheme(
         else -> LightColorScheme
     }
 
-    MaterialTheme(
+    val currentPalette = DefaultGlobalPalette
+
+    CompositionLocalProvider(
+        GlobalColorsPalette provides currentPalette
+    ) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content
+        )
+    }
+
+    /*MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
         content = content
-    )
+    )*/
 }
