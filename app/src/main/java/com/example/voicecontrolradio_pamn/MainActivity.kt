6d65893e36bb29901ebf025d18675153e7aaf665
Agustin-Vizcaino <agustin.vizcaino101@alu.ulpgc.es>
2025-01-13 20:29:24 +0000

    Created background gradient

--- app/src/main/java/com/example/voicecontrolradio_pamn/MainActivity.kt
+++ app/src/main/java/com/example/voicecontrolradio_pamn/MainActivity.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.voicecontrolradio_pamn.ui.theme.VoiceControlRadioPAMNTheme
 import com.example.voicecontrolradio_pamn.ui.theme.app.GlobalColorsPalette
 
@@ -21,14 +32,32 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             VoiceControlRadioPAMNTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                GradientBackground {
+                    Scaffold(modifier = Modifier.fillMaxSize(), containerColor = Color.Transparent) { innerPadding ->
+                        Greeting(
+                            name = "Android",
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                    }
                 }
             }
         }
+        /*setContent {
+            VoiceControlRadioPAMNTheme {
+                Box(modifier = Modifier.fillMaxSize()) {
+                    GradientBackground()
+                    Greeting(
+                        name = "Android"
+                    )
+                }
+
+                /*Scaffold(
+                    Box(modifier = Modifier.padding(innerPadding)) {
+
+                    }
+                }*/
+            }
+        }*/
     }
 }
 
@@ -36,24 +65,41 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Row(modifier = modifier) {
         Text(
-            text = "Hello ",
-            modifier = modifier
+            text = "Hello "
         )
         Text(
             text = name,
-            modifier = modifier,
-            color = GlobalColorsPalette.current.activeText)
+            color = GlobalColorsPalette.current.activeText
+        )
         Text(
-            text = "!",
-            modifier = modifier
+            text = "!"
         )
     }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun GradientBackground(modifier: Modifier = Modifier, Content: @Composable () -> Unit) {
+    val gradientBrush = Brush.verticalGradient(
+        0.5f to GlobalColorsPalette.current.backgroundStart,
+        1.0f to GlobalColorsPalette.current.backgroundEnd
+        /*colors = listOf(
+            GlobalColorsPalette.current.backgroundStart,
+            GlobalColorsPalette.current.backgroundEnd
+        ),
+        startY = 1000f*/
+    )
+
+    Box(modifier = Modifier.fillMaxSize().background(gradientBrush)) {
+        Content()
+    }
+}
+
+/*@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     VoiceControlRadioPAMNTheme {
-        Greeting("Android")
+        GradientBackground {
+            Greeting("Android")
+        }
     }
-}
+}*/
