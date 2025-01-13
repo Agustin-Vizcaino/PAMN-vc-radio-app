cc6e6790ecc27d02c4ba5ec7c4ad86fccaf0abd4
Agustin-Vizcaino <agustin.vizcaino101@alu.ulpgc.es>
2025-01-13 22:00:23 +0000

    Added icon, but no idea how to select the right resolution

--- app/src/main/java/com/example/voicecontrolradio_pamn/MainActivity.kt
+++ app/src/main/java/com/example/voicecontrolradio_pamn/MainActivity.kt
@@ -5,22 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.voicecontrolradio_pamn.ui.theme.VoiceControlRadioPAMNTheme
@@ -36,8 +40,9 @@ class MainActivity : ComponentActivity() {
                     Scaffold(modifier = Modifier.fillMaxSize(), containerColor = Color.Transparent) { innerPadding ->
                         Box(modifier = Modifier.padding(innerPadding)) {
                             AppFrame {
+                                CenteredLogo()
                                 Greeting(
-                                    name = "Androidd",
+                                    name = "Android",
                                     //modifier = Modifier.padding(innerPadding)
                                 )
                             }
@@ -125,6 +130,17 @@ fun AppFrame(Content: @Composable () -> Unit) {
         }
     }
 
+}
+
+@Composable
+fun CenteredLogo() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Image(
+            painter = painterResource(id = R.mipmap.logo),
+            contentDescription = "Centered Logo",
+            modifier = Modifier.fillMaxSize(0.66f)
+        )
+    }
 }
 
 /*@Preview(showBackground = true)
