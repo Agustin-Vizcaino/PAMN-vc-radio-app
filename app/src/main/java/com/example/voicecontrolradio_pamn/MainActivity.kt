100446793440f0d3baef4e783f74a571dda6c6e8
Agustin-Vizcaino <agustin.vizcaino101@alu.ulpgc.es>
2025-01-14 14:17:57 +0000

    Added basic code for speech recognition

--- app/src/main/java/com/example/voicecontrolradio_pamn/MainActivity.kt
+++ app/src/main/java/com/example/voicecontrolradio_pamn/MainActivity.kt
@@ -1,23 +1,35 @@
 package com.example.voicecontrolradio_pamn
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
@@ -27,12 +39,23 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.Manifest
 import com.example.voicecontrolradio_pamn.ui.theme.VoiceControlRadioPAMNTheme
 import com.example.voicecontrolradio_pamn.ui.theme.app.GlobalColorsPalette
+import java.util.Locale
+
+val language = arrayOf("es-ES", Locale.getDefault())
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), 1)
+        }
+
         enableEdgeToEdge()
         setContent {
             VoiceControlRadioPAMNTheme {
@@ -45,6 +68,7 @@ class MainActivity : ComponentActivity() {
                                     name = "Androidd",
                                     //modifier = Modifier.padding(innerPadding)
                                 )
+                                MainScreen(modifier = Modifier.padding(innerPadding))
                             }
                         }
                     }
@@ -140,6 +164,39 @@ fun CenteredLogo() {
             contentDescription = "Centered Logo",
             modifier = Modifier.fillMaxSize(0.66f)
         )
+    }
+}
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+    val speechText = remember { mutableStateOf("Your speech will appear here.") }
+    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == Activity.RESULT_OK) {
+            val data = it.data
+            val result = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            speechText.value = result?.get(0) ?: "No speech detected."
+        } else {
+            speechText.value = "[Speech recognition failed.]"
+        }
+    }
+    Column(modifier = modifier
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = {
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            //intent.putExtra("android.speech.extra.EXTRA_ADDITIONAL_LANGUAGES", language);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "es-ES")
+            //intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Go on then, say something.")
+            launcher.launch(intent)
+        }) {
+            Text("Start speech recognition")
+        }
+        Spacer(modifier = Modifier.padding(16.dp))
+        Text(speechText.value)
     }
 }
 
